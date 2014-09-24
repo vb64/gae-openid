a3e326bf8b86154043f017dd93014ea54cdd1b5e
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2014-09-24 14:10:22 +0400

    try suppress

--- gaeopenid/urls.py
+++ gaeopenid/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-  url(r'^start/$', views.OpenIDStartSubmit, name='openid_start'),
+  url(r'^start/$', views.OpenIDStartSubmit, {}, name='openid_start'),
   url(r'^finish/$', views.OpenIDFinish, name='openid_finish'),
-  url(r'^rpxrds/$', views.RelyingPartyXRDS),
-  url(r'^error/(?P<err_id>.*)/$', views.ErrorPage),
+  url(r'^rpxrds/$', views.RelyingPartyXRDS, {}),
+  url(r'^error/(?P<err_id>.*)/$', views.ErrorPage, {}),
 )
