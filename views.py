27909291fd8044e47a31f5b1c6a2c5a3aaff36a1
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2014-09-24 14:27:24 +0400

    fix syntax

--- views.py
+++ views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render_to_response
 def mainpage(request):
     return render_to_response('main.html')
 
-def success_handler(request, response, openid_url)
+def success_handler(request, response, openid_url):
     logging.warning("success_handler openid_url: %s" % openid_url)
