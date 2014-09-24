7d24635bd7c3283d7de7d51d3a564cd70dabe51a
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2014-09-24 13:34:16 +0400

    fix syntax

--- gaeopenid/urls.py
+++ gaeopenid/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import *
+
 import views
 
 urlpatterns = patterns('',
-  (r'^start/$', views.OpenIDStartSubmit, name='openid_start'),
-  (r'^finish/$', views.OpenIDFinish, name='openid_finish'),
-  (r'^rpxrds/$', views.RelyingPartyXRDS),
-  (r'^error/(?P<err_id>.*)/$', views.ErrorPage),
+  url(r'^start/$', views.OpenIDStartSubmit, name='openid_start'),
+  url(r'^finish/$', views.OpenIDFinish, name='openid_finish'),
+  url(r'^rpxrds/$', views.RelyingPartyXRDS),
+  url(r'^error/(?P<err_id>.*)/$', views.ErrorPage),
 )
